3b0acd26beec57fbddd9f2550faee4d12cda0da9
ak2788 <afsheenk654@gmail.com>
2026-02-27 23:34:20 -0500

    Initial commit

--- app/src/main/java/com/codepath/campgrounds/MainActivity.kt
+++ app/src/main/java/com/codepath/campgrounds/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.campgrounds
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.campgrounds.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -21,8 +24,7 @@ fun createJson() = Json {
 
 private const val TAG = "CampgroundsMain/"
 private val PARKS_API_KEY = BuildConfig.API_KEY
-private val CAMPGROUNDS_URL =
-    "https://developer.nps.gov/api/v1/campgrounds?api_key=${PARKS_API_KEY}"
+private val CAMPGROUNDS_URL = "https://developer.nps.gov/api/v1/campgrounds?api_key=${PARKS_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var campgroundsRecyclerView: RecyclerView
@@ -71,16 +73,25 @@ class MainActivity : AppCompatActivity() {
                     )
 
                     // TODO: Do something with the returned json (contains campground information)
-                    parsedJson.data?.let { list ->
-                        campgrounds.addAll(list)
-                    }
+//                    parsedJson.data?.let { list ->
+//                        (application as CampgroundApplication).db.campgroundDao().deleteAll()
+//                        (application as CampgroundApplication).db.campgroundDao().insertAll(TODO)
+//                    }
 
                     // TODO: Save the campgrounds and reload the screen
                     parsedJson.data?.let { list ->
-                        campgrounds.addAll(list)
-
-                        // Notify the adapter that the dataset has changed
-                        campgroundAdapter.notifyDataSetChanged()
+                        lifecycleScope.launch(IO) {
+                            (application as CampgroundApplication).db.campgroundDao().deleteAll()
+                            (application as CampgroundApplication).db.campgroundDao()
+                                .insertAll(list.map {
+                                    CampgroundEntity(
+                                        name = it.name,
+                                        description = it.description,
+                                        latLong = it.latLong,
+                                        imageUrl = it.imageUrl
+                                    )
+                                })
+                        }
                     }
 
                 } catch (e: JSONException) {
@@ -89,5 +100,25 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        // TODO: Step 7: Load new items from our database
+        lifecycleScope.launch {
+            (application as CampgroundApplication).db.campgroundDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    Campground(
+                        entity.name,
+                        entity.description,
+                        entity.latLong,
+                        listOf(CampgroundImage(entity.imageUrl, null))
+                    )
+                }.also { mappedList ->
+                    campgrounds.clear()
+                    campgrounds.addAll(mappedList)
+                    campgroundAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+
     }
 }
